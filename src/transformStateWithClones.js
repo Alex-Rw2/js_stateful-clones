c8ae507ed61ff5bdbbf902f70d723cb9370a88a5
Alex Rodrigues <alexrodriguesw2@hotmail.com>
2026-01-02 21:40:56 -0300

    first update

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const copy = { ...state };
 
+  if (actions.type === 'addProperties') {
+    if (typeof actions.extraData === 'object' && actions.extraData !== null) {
+      Object.assign(copy, actions.extraData);
+    }
+  }
+
+  if (actions.type === 'removeProperties') {
+    if (Array.isArray(actions.keysToRemove)) {
+      for (const key of actions.keysToRemove) {
+        delete copy[key];
+      }
+    }
+  }
+
+  if (actions.type === 'clear') {
+    Object.keys(copy).forEach((key) => delete copy[key]);
+  }
+
+  return copy;
+}
 module.exports = transformStateWithClones;
